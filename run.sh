26332b63223326a45a03732b18af4289a1843309
Vali Malinoiu <0x4139@gmail.com>
2015-04-01 20:48:05 +0300

    Update run.sh
    broadcast address needed to run cassandra behind containers

--- run.sh
+++ run.sh
@@ -23,6 +23,7 @@ if [ ! -f /root/.cassconfig ]; then
     sed -i -e "s/cluster_name.*/cluster_name: '0x4139'/"  $CONF_FILE
     sed -i -e "s/listen_address.*/listen_address: $ADDR/"  $CONF_FILE
     sed -i -e "s/broadcast_rpc_address.*/broadcast_rpc_address: $PUBLIC/"  $CONF_FILE
+    sed -i -e "s/\# broadcast_address.*/broadcast_address: $PUBLIC/"  $CONF_FILE
     sed -i -e "s/- seeds.*/- seeds: \"$SEED\"/"  $CONF_FILE
     sed -i -e "s/rpc_address.*/rpc_address: $ADDR/"  $CONF_FILE
 
